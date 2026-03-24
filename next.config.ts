67f5c425d2d389ae43b6cf49451ed79b8d591cd5
Damon Walker <dwalker@sumglobal.com>
2026-03-24 16:40:20 -0400

    Vercel setup

--- next.config.ts
+++ next.config.ts
@@ -4,5 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: false, // Ensure all routes end with a slash
 };
-
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/calc-vault/:path*",
+        destination: "http://localhost:3001/calc-vault/:path*", // Proxy to Backend
+      },
+    ];
+  },
+}
 export default nextConfig;
